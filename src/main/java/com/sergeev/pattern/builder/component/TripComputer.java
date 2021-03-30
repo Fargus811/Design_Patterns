e9a80d4f1697d623d7cb077d79852eccf817e603
Daniil Siarheyeu <daniil.siarheyeu@itrexgroup.lan>
2021-03-30 16:05:17 +0300

    builder

---
+++ src/main/java/com/sergeev/pattern/builder/component/TripComputer.java
@@ -0,0 +1,27 @@
+package main.java.com.sergeev.pattern.builder.component;
+
+import main.java.com.sergeev.pattern.builder.car.Car;
+
+/**
+ * Одна из фишек автомобиля.
+ */
+public class TripComputer {
+
+    private Car car;
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void showFuelLevel() {
+        System.out.println("Fuel level: " + car.getFuel());
+    }
+
+    public void showStatus() {
+        if (this.car.getEngine().isStarted()) {
+            System.out.println("Car is started");
+        } else {
+            System.out.println("Car isn't started");
+        }
+    }
+}
---
+++ src/main/java/com/sergeev/pattern/builder/example_builder/CarBuilder.java
@@ -0,0 +1,53 @@
+package main.java.com.sergeev.pattern.builder.example_builder;
+
+import main.java.com.sergeev.pattern.builder.car.Car;
+import main.java.com.sergeev.pattern.builder.car.CarType;
+import main.java.com.sergeev.pattern.builder.component.Engine;
+import main.java.com.sergeev.pattern.builder.component.GPSNavigator;
+import main.java.com.sergeev.pattern.builder.component.Transmission;
+import main.java.com.sergeev.pattern.builder.component.TripComputer;
+
+/**
+ * Конкретные строители реализуют шаги, объявленные в общем интерфейсе.
+ */
+public class CarBuilder implements Builder {
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Car getResult() {
+        return new Car(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
