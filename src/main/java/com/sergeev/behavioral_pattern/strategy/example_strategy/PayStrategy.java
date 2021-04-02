228b56d2d17ef36fbd8908140b174c433cbaf362
Daniil Siarheyeu <daniil.siarheyeu@itrexgroup.lan>
2021-04-02 10:29:04 +0300

    strategy

---
+++ src/main/java/com/sergeev/behavioral_pattern/strategy/example_strategy/PayStrategy.java
@@ -0,0 +1,9 @@
+package main.java.com.sergeev.behavioral_pattern.strategy.example_strategy;
+
+/**
+ * Общий интерфейс всех стратегий.
+ */
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
+}
